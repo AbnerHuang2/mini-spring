062ba4b428e8e97b5ac58e62bccaf5889aaa2858
huanghousheng <huanghousheng@cvte.com>
2023-10-11 20:18:14 +0800

    init

---
+++ spring-ioc/src/main/java/org/skitii/Main.java
@@ -0,0 +1,13 @@
+package org.skitii;
+
+/**
+ * @author skitii
+ * @since 2023/10/10
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
---
+++ spring-ioc/src/main/java/org/skitii/factory/BeanFactory.java
@@ -0,0 +1,10 @@
+package org.skitii.factory;
+
+/**
+ * @author skitii
+ * @since 2023/10/10
+ **/
+public interface BeanFactory {
+
+    public Object getBean();
+}
